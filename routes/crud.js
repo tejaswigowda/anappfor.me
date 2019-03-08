7b16b19e56747c41ee7f5d1d23b010431da6f858
tejaswigowda <tejaswi@foxyninjastudios.com>
2019-03-07 22:17:23 -0700

    ac

--- routes/crud.js
+++ routes/crud.js
@@ -62,8 +62,7 @@ router.get('/edit', isLoggedIn, function(req, res) {
 
 module.exports = router
 
-// route middleware to ensure user is logged in
-function isLoggedIn(req, res, next) {
+function isLoggedIn(req, res, next) {// route middleware to ensure user is logged in
     if (req.isAuthenticated())
         return next();
     res.send('noauth');
