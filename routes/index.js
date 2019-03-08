0d8db1ecf3ecb9109dfbb32ed5fee520dedf06ae
tejaswigowda <tejaswi@foxyninjastudios.com>
2019-03-07 21:24:32 -0700

    crud + projects + userid

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 var express = require('express')
   , router = express.Router()
 
-router.use('/projects', require('./projects'))
+router.use('/projects', require('./crud'));
+router.use('/userid', require('./crud'));
+
+/*router.get("/getall", isLoggedIn, function(req, res){
+  res.send("sdjbc");
+});*/
 
 module.exports = router
+
+function isLoggedIn(req, res, next) {// route middleware to ensure user is logged in
+    if (req.isAuthenticated())
+        return next();
+    res.send('noauth');
+}
